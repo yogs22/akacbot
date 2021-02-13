f14eabf7cebcb68e93233eb6ad3e2e5b08bc5c27
yogs22 <yogi.enjoyers22@gmail.com>
2021-02-13 23:17:24 +0700

    Add delete confirmation

--- resources/views/livewire/lessons/index.blade.php
+++ resources/views/livewire/lessons/index.blade.php
@@ -38,7 +38,7 @@
                             <td class="border px-4 py-2">{{ $row->code }}</td>
                             <td class="border px-4 py-2">
                                 <button wire:click="edit({{ $row->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-                                <button wire:click="delete({{ $row->id }})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Hapus</button>
+                                <button onclick="confirm('Yakin akan menghapus data ini?') || event.stopImmediatePropagation()" wire:click="delete({{ $row->id }})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Hapus</button>
                             </td>
                         </tr>
                     @empty
