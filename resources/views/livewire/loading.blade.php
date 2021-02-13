a0db301b092bc592c98dd3d265ea1178f259ea05
yogs22 <yogi.enjoyers22@gmail.com>
2021-02-13 21:14:20 +0700

    Add loading progress

---
+++ resources/views/livewire/loading.blade.php
@@ -0,0 +1,14 @@
+<div wire:loading wire:target="create, edit, delete, store, closeModal, picture">
+    <div class="block-loading">
+        <div>
+            <div class="bounce">
+                <div class="bounce1"></div>
+                <div class="bounce2"></div>
+                <div class="bounce3"></div>
+            </div>
+            <div class="mt-3">
+                Sedang memproses ...
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/majors/index.blade.php
+++ resources/views/livewire/majors/index.blade.php
@@ -4,6 +4,7 @@
     </h2>
 </x-slot>
 <div class="py-12">
+    @include('livewire.loading')
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
             @if (session()->has('message'))
